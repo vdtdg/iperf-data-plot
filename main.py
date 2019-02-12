a361cfe74c4aa2944912eafbc39f74b21f19a3f3
vdtdg <v.dethezan@gmail.com>
2019-02-12 23:42:45 +0100

    added args support

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import talib as ta
 import numpy as np
+import sys
+import argparse
 
 
 def print_file_info(data):
@@ -10,35 +12,61 @@ def print_file_info(data):
     pprint(data['end'])
 
 
-def tcp_chart(data):
+def chart(args, data):
+    # Setting the default values
+    expected_bandwidth = 0
+    ema = 60
+    if args.protocol == 'udp':
+        sum_string = 'sum'
+    else:
+        sum_string = 'sum_sent'
+    if args.ema is not None:
+        ema = int(args.ema)
+    if args.expectedbw is not None:
+        expected_bandwidth = int(args.expectedbw)
+
     debit = []
     intervals = data['intervals']
     for i in intervals:
         debit.append(i['sum']['bits_per_second'])
 
     plt.plot(debit, label='bandwitdh per second')
-    plt.axhline(data['end']['sum_sent']['bits_per_second'], color='r', label='Avg bandwidth')
-    plt.title("{}, {}, 1GB file".format(data['start']['timestamp']['time'], data['start']['test_start']['protocol']))
+
+    plt.axhline(data['end'][sum_string]['bits_per_second'], color='r', label='Avg bandwidth')
+    plt.axhline(expected_bandwidth * 1000000, color='g', label='Expected bandwidth')
+    plt.plot(ta.EMA(np.array(debit), ema), label='bandwitch {} period moving average'.format(ema))
+
+    plt.title("{}, {}, {:.3}GB file".format(data['start']['timestamp']['time'],
+                                         data['start']['test_start']['protocol'],
+                                         data['end'][sum_string]['bytes']/1000000000))
     plt.legend()
+    plt.ylim(bottom=0)
+    plt.ylabel('bit/s')
+    plt.xlabel('time interval')
     plt.show()
 
 
-def udp_chart(data):
-    debit = []
-    intervals = data['intervals']
-    for i in intervals:
-        debit.append(i['sum']['bits_per_second'])
+def main(argv):
+    parser = argparse.ArgumentParser(description='Simple python iperf JSON data vizualiser.')
+    parser.add_argument('input', nargs='?', help='JSON output file from iperf')
+    parser.add_argument('-a', '--ema', help='Exponential moving average used to smooth the bandwidth. Default at 60.', type=int)
+    parser.add_argument('-e', '--expectedbw', help='Expected bandwidth to be plotted in Mb.')
+    parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
+    args = parser.parse_args(argv)
+    with open(args.input) as f:
+        data = json.load(f)
+        if args.verbose:
+            print_file_info(data)
+        if data['start']['test_start']['protocol'] == 'UDP':
+            args.protocol = 'udp'
+        else:
+            args.protocol = 'tcp'
+        chart(args, data)
 
-    plt.plot(debit, label='bandwitdh per second')
-    plt.axhline(data['end']['sum']['bits_per_second'], color='r', label='Avg bandwidth')
-    plt.plot(ta.EMA(np.array(debit), 60), label='bandwitch 60 period moving average')
 
-    plt.title("{}, {}, 1GB file".format(data['start']['timestamp']['time'], data['start']['test_start']['protocol']))
-    plt.legend()
-    plt.show()
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
-
-with open('output.json') as f:
-    data = json.load(f)
-    print_file_info(data)
-    udp_chart(data)
+# todo : remove ta lib requirement
+# todo : test with a tcp output file
+# todo : add more info in the print_file_info and to the verbose fonction
